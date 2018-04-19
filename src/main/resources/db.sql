c889b35908ae4ed09c47666d4100e67a641b0a37
David Karchikyan <david.karchikyan@gmail.com>
2018-04-19 22:07:19 +0400

    add db.sql for create tables

---
+++ src/main/resources/db.sql
@@ -0,0 +1,11 @@
+/* Create Users table */
+CREATE TABLE users (
+  id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  login VARCHAR(100) NOT NULL UNIQUE,
+  passhash VARCHAR(100) NOT NULL,
+  firstname VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  role INT NOT NULL
+)
+  ENGINE = InnoDB;
