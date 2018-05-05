4ccf644a3f28163032773fd6c72e9df5128cc39a
David Karchikyan <david.karchikyan@gmail.com>
2018-05-05 17:25:54 +0400

    add ProductType Service

---
+++ src/main/java/am/lavshuka/lad/service/product/ProductTypeService.java
@@ -0,0 +1,29 @@
+package am.lavshuka.lad.service.product;
+
+import am.lavshuka.lad.dao.product.ProductTypeDao;
+import am.lavshuka.lad.model.product.ProductCategory;
+import am.lavshuka.lad.model.product.ProductType;
+
+import java.util.List;
+
+/**
+ * Created by David on 5/5/2018.
+ */
+public class ProductTypeService {
+
+    public void addProductType(ProductType productType) {
+        new ProductTypeDao().add(productType);
+    }
+
+    public ProductType findProductTypeByName(String typeName) {
+        return new ProductTypeDao().findByTypeName(typeName);
+    }
+
+    public List<ProductType> findAllProductType() {
+        return new ProductTypeDao().findAll(ProductType.class);
+    }
+
+    public List<ProductType> findProductTypeByCategory() {
+        return (List<ProductType>) new ProductCategory().getProductTypeSet();
+    }
+}
